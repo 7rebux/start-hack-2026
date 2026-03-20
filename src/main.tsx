c127873b3c7d756c695c868814822c788ab5319d
Noah Gerber <noahgerber100@gmail.com>
2026-03-20 01:47:23 +0100

    fix graph structure via toolcalls

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import App from './App.tsx'
 import { ExpertBookingPage } from './pages/ExpertBookingPage.tsx'
 import { BookingSuccessPage } from './pages/BookingSuccessPage.tsx'
 import { TopicViewPage } from './pages/TopicViewPage.tsx'
+import { ResearchPage } from './pages/ResearchPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -15,6 +16,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/schedule/:expertId" element={<ExpertBookingPage />} />
         <Route path="/booking-success" element={<BookingSuccessPage />} />
         <Route path="/topic/:topicId" element={<TopicViewPage />} />
+        <Route path="/research" element={<ResearchPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
